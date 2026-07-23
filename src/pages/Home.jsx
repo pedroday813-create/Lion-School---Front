13106df5797b70d4013ff73d64c30e349cf89f6c
PEDRO RODRIGUES CRUZ <25203773@dsn127m1242314.cfp127aluno.local>
2026-07-23 11:21:58 -0300

    fazendo o header da Land Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import VectorImg from '../assets/Vector.png'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Bem-vindo à Lion School! 🦁</h1>
-      <p>Sua escola técnica de tecnologia.</p>
+    <div className="home-header">
+      <img className="home-logo" src={VectorImg} alt="Vector" />
+      <h1 className="home-title">Lion <br />School</h1>
+      <h1 className="home-logout">Sair</h1>
     </div>
   )
 }
