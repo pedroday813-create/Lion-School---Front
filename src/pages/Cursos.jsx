123d4e8b9c2669aaf42dec01b8e90bde0ab3dcaf
pedro <pedroday813@gmail.com>
2026-07-30 11:26:19 -0300

    Em Desenvolvimento

--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -1,8 +1,39 @@
+import './index.css'
+
+
 export default function Cursos() {
   return (
     <>
-      
-      
+      <header className="home-header">
+        <div className="home-left">
+          <img
+            src={VectorImg}
+            alt="Logo Lion School"
+            className="home-logo"
+          />
+
+          <h1 className="home-title">
+            Lion
+            <br />
+            School
+          </h1>
+        </div>
+
+        <div className="home-right">
+          <img
+            src={LogoutImg}
+            alt="Sair"
+            className="logout-icon"
+          />
+
+            <a href="http://google.com.br">
+          <button className="home-logout">
+            Sair
+          </button>
+          </a>
+        </div>
+      </header>
+
     
     </>
   )
